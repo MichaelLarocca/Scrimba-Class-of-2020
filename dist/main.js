8a9c9f85565a1e7212ac7837e3097691946af46b
Michael Larocca <michaellarocca@verizon.net>
2020-10-11 21:15:29 -0400

    Initial commit

---
+++ dist/main.js
@@ -0,0 +1,20 @@
+function btnInfo_mjl() {
+  const userInfo = document.getElementById("info-mjl");
+  userInfo.style.display === "none"
+    ? (userInfo.style.display = "block")
+    : (userInfo.style.display = "none");
+}
+
+function btnInfo_janeDoe() {
+  const userInfo = document.getElementById("info-janeDoe");
+  userInfo.style.display === "none"
+    ? (userInfo.style.display = "block")
+    : (userInfo.style.display = "none");
+}
+
+function btnInfo_johnDoe() {
+  const userInfo = document.getElementById("info-johnDoe");
+  userInfo.style.display === "none"
+    ? (userInfo.style.display = "block")
+    : (userInfo.style.display = "none");
+}
